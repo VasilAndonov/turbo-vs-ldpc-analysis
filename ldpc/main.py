b83d70abff75793f69a1cd013277054ae614ccb6
vasil-andonov <54496434+VasilAndonov@users.noreply.github.com>
2026-03-14 12:16:55 +0200

    Create main.py

---
+++ ldpc/main.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+FAST_MODE = True
+SHOW_PLOTS = True
+SAVE_PLOTS = False
+PLOT_PREFIX = "ldpc"
+
+RANDOM_SEED = 12
+INFORMATION_BITS = 256 if FAST_MODE else 1024
+
+ITERATIONS = [1, 2, 3, 4, 5, 6]
+LDPC_EBN0_DB = np.array([-1.0, 0.0, 0.8, 1.0, 1.15, 1.25, 1.3], dtype=float) if FAST_MODE else np.arange(-1.0, 1.36, 0.15)
+
+MIN_FRAMES = 20 if FAST_MODE else 120
+MAX_FRAMES = 70 if FAST_MODE else 400
+TARGET_ERRORS = 80 if FAST_MODE else 300
+BENCHMARK_BLOCKS = 8 if FAST_MODE else 24
+
+SUPPORTED_CODE_RATES = {"1/3": 1/3, "1/2": 1/2, "3/4": 3/4, "7/8": 7/8}
+SELECTED_CODE_RATE_LABEL = "1/3"
+
+def sigma2_from_ebn0(ebn0_db, code_rate):
+    ebn0 = 10.0 ** (ebn0_db / 10.0)
+    return 1.0 / (2.0 * code_rate * ebn0)
